60b4856b70e3a90d10e888e9c797eed729cc3899
Max <maks200179@mail.ru>
2019-11-13 13:22:45 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,18 +4,6 @@ set -x
 
 DIR=$(dirname "$(readlink -f "$0")")
 
-
-
-
-
-
-	
-git_dir="/puppet_data/.git"
-if [[ -d ${git_dir} ]]; then 
-    rm -fr "${git_dir}"
-    echo "git dir removed"
-fi	
-	
 	
 if [[ ! $(docker ps -a | grep 'powershell') ]] ; then
     docker-compose  -f "${DIR}"/docker_env/docker-compose.yml up -d --build powershell
