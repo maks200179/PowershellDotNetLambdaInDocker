a8aa3933eb72973b9ce6032470914c7eb4307704
Max <maks200179@mail.ru>
2019-11-14 02:34:29 +0200

    Create Pwsh_find_a_group_rds_function_version.ps1

---
+++ docker_env/app/Pwsh_find_a_group_rds_function_version.ps1
@@ -0,0 +1,58 @@
+function Get-AWSRDSDetails 
+{
+    [CmdletBinding()]
+    Param 
+    (
+            [parameter(Mandatory=$true)]   
+            [string] $VpcSecurityGroupIDToRemove
+    )
+    
+    $rulesRemoved = 0
+
+    try {
+        $RDSInstances = Get-RDSDBInstance 
+    } catch {
+        $ErrorMessage = $_.Exception.Message
+        Write-Warning "Get-AWSRDSDetails - Error: $ErrorMessage"
+        return
+    }
+    
+    foreach ($instance in $RDSInstances) 
+    {
+        [array]$VpcGroupID    = ($instance.VpcSecurityGroups.VpcSecurityGroupId).Trim() 
+        $Environment          = Get-RDSTagForResource  $instance.DBInstanceArn | Where-Object {$_.key -eq "Phase"} | Select-Object -Expand Value
+        $DBInstanceIdentifier = ($instance.DBInstanceIdentifier | ft -HideTableHeaders | Out-String).Trim()
+        
+        if(($VpcGroupID -Contains $VpcSecurityGroupIDToRemove) -and ($Environment -eq "Prod")) 
+        {
+    
+            if($VpcGroupID.Count -gt 1) 
+            {
+                [System.Collections.ArrayList]$VpcGroupIDEdited = $VpcGroupID
+                $VpcGroupIDEdited.Remove($VpcSecurityGroupIDToRemove)
+            }
+                
+            else
+            {
+                $VpcGroupIDEdited = $VpcGroupID
+            }
+                
+            try 
+            {
+                Edit-RDSDBInstance -DBInstanceIdentifier $DBInstanceIdentifier -VpcSecurityGroupId $VpcGroupIDEdited -Force -ErrorAction stop
+            }   catch 
+            {
+                $ErrorMessage = $_.Exception.Message
+                Write-Error "Edit-RDSDBInstance - Error: $ErrorMessage"
+                Break
+            }    
+                $rulesRemoved++
+                Write-Host "CountRulesRemoved : " $rulesRemoved
+        }
+        
+    }
+   
+}
+
+
+Get-AWSRDSDetails  $LambdaInput.VpcSecurityGroupVar
